ea54bd12d961425b05a8e6bf9492cc5fd3844e1e
fahminurcahya <fahminurcahya@gmail.com>
2023-02-05 13:50:11 +0700

    payments

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const organizersRouter = require("./app/api/v1/organizers/router");
 const eventsRouter = require("./app/api/v1/events/router");
 const ordersRouter = require("./app/api/v1/orders/router");
 const participantsRouter = require("./app/api/v1/participants/router");
+const paymentsRouter = require("./app/api/v1/payments/router");
 
 const v1 = "/api/v1/cms";
 
@@ -37,6 +38,7 @@ app.use(`${v1}/images`, imagesRouter);
 app.use(`${v1}/talents`, talentsRouter);
 app.use(`${v1}/events`, eventsRouter);
 app.use(`${v1}/orders`, ordersRouter);
+app.use(`${v1}/payments`, paymentsRouter);
 app.use(`${v1}/auth`, authCMSRouter);
 app.use("/api/v1", participantsRouter);
 
